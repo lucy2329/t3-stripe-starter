1b337a4f70c13f188e50be4ebc899b3f8d9fb2be
Vishal Sathyanarayana <vishal23dps@gmail.com>
2023-08-01 20:36:52 -0700

    feat: stripe webhook done

---
+++ src/pages/api/webhook/index.ts
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { buffer } from "micro"; //TODO replace with react query
+import Cors from "micro-cors";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import Stripe from "stripe";
+
+import { type RequestHandler } from "next/dist/server/next";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: "2022-11-15",
+});
+
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
+
+// Stripe requires the raw body to construct the event.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const cors = Cors({
+  allowMethods: ["POST", "HEAD"],
+});
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"]!;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf.toString(),
+        sig,
+        webhookSecret
+      );
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : "Unknown error";
+      // On error, log and return the error message.
+      if (err instanceof Error) console.log(err);
+      console.log(`❌ Error message: ${errorMessage}`);
+      res.status(400).send(`Webhook Error: ${errorMessage}`);
+      return;
+    }
+
+    // Successfully constructed event.
+    console.log("✅ Success:", event.id, event.type);
+
+    // Cast event data to Stripe object.
+    switch (event.type) {
+      case "checkout.session.completed":
+        // Payment is successful and the subscription is created.
+        // You should provision the subscription and save the customer ID to your database.
+        console.log("✅ Checkout session completed!", event.data.object);
+        break;
+      case "invoice.paid":
+        // Continue to provision the subscription as payments continue to be made.
+        // Store the status in your database and check when a user accesses your service.
+        // This approach helps you avoid hitting rate limits.
+        console.log("✅ Invoice paid!", event.data.object);
+        break;
+      case "invoice.payment_failed":
+        // The payment failed or the customer does not have a valid payment method.
+        // The subscription becomes past_due. Notify your customer and send them to the
+        // customer portal to update their payment information.
+        console.log("❌ Invoice payment failed!", event.data.object);
+        break;
+      default:
+        // Unhandled event type; handle whatever event you need. refer to stripe docs for more info on all event types
+        console.log(`❌ Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event.
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
+
+export default cors(webhookHandler as RequestHandler);
